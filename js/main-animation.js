dcbf1fd329261ef452cd6c94673bd40996e3c8ed
mitoo <mitoolab@gmail.com>
2017-04-13 11:18:48 +0900

    로그인, 회원가입 에러메서지 추가

--- js/main-animation.js
+++ js/main-animation.js
@@ -2,17 +2,33 @@
 //album animation
 	var contents = document.querySelectorAll('.contents-wrap');
 
-	for( var i in contents) {
-		contents[i].addEventListener('mouseover', function() {
+	contents.forEach(function(content, index){
+
+		content.addEventListener('mouseover', function() {
 			var image = this.parentNode.children[1].firstElementChild;
 			this.setAttribute('style','top: -30px; transition: all 0.3s ease-in-out;');
 			image.setAttribute('style','transform: scale(1.15); transition: all 0.5s ease-in-out;');
 		});
-		contents[i].addEventListener('mouseout', function() {
+
+		content.addEventListener('mouseout', function() {
 			var image = this.parentNode.children[1].firstElementChild;
 			this.setAttribute('style','top: 0px; transition: all 0.3s ease-in-out;');
 			image.setAttribute('style','transform: scale(1); transition: all 0.5s ease-in-out;');
 		});
-	}
+
+	});
+
+	// for( var i in contents) {
+	// 	contents[i].addEventListener('mouseover', function() {
+	// 		var image = this.parentNode.children[1].firstElementChild;
+	// 		this.setAttribute('style','top: -30px; transition: all 0.3s ease-in-out;');
+	// 		image.setAttribute('style','transform: scale(1.15); transition: all 0.5s ease-in-out;');
+	// 	});
+	// 	contents[i].addEventListener('mouseout', function() {
+	// 		var image = this.parentNode.children[1].firstElementChild;
+	// 		this.setAttribute('style','top: 0px; transition: all 0.3s ease-in-out;');
+	// 		image.setAttribute('style','transform: scale(1); transition: all 0.5s ease-in-out;');
+	// 	});
+	// }
 
 var header = document.querySelector('.header');
--- js/main-page.js
+++ js/main-page.js
@@ -1,56 +1,150 @@
-// sign-in, sign-up
 
 // sign-in-modal
 var modal_sign_in = document.querySelector(".sign-in-modal");
 var btn_sign_in = document.querySelector(".sign-in");
+var sign_in_wrap = document.querySelector(".sign-in-wrap");
+var button_create_account = document.querySelector(".button-create-account");
 
-btn_sign_in.onclick = function() {
-    modal_sign_in.style.display = "block";
-}
 
 // sign-up-modal
 var modal_sign_up = document.querySelector(".sign-up-modal");
 var btn_sign_up = document.querySelector(".sign-up");
+var sign_up_wrap = document.querySelector(".sign-up-wrap");
+
+//  open button
+btn_sign_in.onclick = function() {
+    // modal_sign_in.style.display = "block";
+    openModal(modal_sign_in);
+}
 
 btn_sign_up.onclick = function() {
-  modal_sign_up.style.display = "block";
+  // modal_sign_up.style.display = "block";
+  openModal(modal_sign_up);
+}
+
+// 모달창 클릭 이벤트 전파 차단
+sign_in_wrap.onclick = function(e) {
+  // console.log('clicked signWrap');
+  e.stopPropagation();
+}
+
+sign_up_wrap.onclick = function(e) {
+  // console.log('clicked signWrap');
+  e.stopPropagation();
+}
+
+// 로그인 모달에서 회원가입 모달로 이동하는 버튼
+button_create_account.onclick = function() {
+  changeAccountMode();
+}
+
+function changeAccountMode() {
+  closeModal.call(modal_sign_in);
+  openModal(modal_sign_up);
 }
 
 // closed button
-
 var btn_closed = document.querySelectorAll(".button-closed");
-for (var i=0; i<=btn_closed.length; i=i+1){
-  // console.log(btn_closed[i]);
-  // onclick = false {
+
+function openModal(target) {
+  target.style.display = 'block';
 }
 
-// var button_sign_in_closed = document.getElementById("button-sign-in-closed");
+function closeModal() {
+  this.style.display = 'none';
+}
+
+btn_closed.forEach(function(item) {
+  var target = item.parentNode.parentNode.parentNode.parentNode;
+  target.onclick = item.onclick = closeModal.bind(target);
+});
+
+
+
+
+
+// jQuery
 //
-// button_sign_in_closed.onclick = function() {
-//   modal_sign_in.style.display = "none";
-// }
+// (function(global, $) {
+//   'use strict';
+//   // sign-in, sign-up
+//   var modal = $(".modal");
+//   // sign-in-modal
+//   var modal_sign_in = $(".sign-in-modal");
+//   var btn_sign_in = $(".sign-in");
+//   // sign-up-modal
+//   var modal_sign_up = $(".sign-up-modal");
+//   var btn_sign_up = $(".sign-up");
+//   //  open button
+//   btn_sign_in.on('click',function() {
+//       // modal_sign_in.style.display = "block";
+//       openModal(modal_sign_in);
+//   });
 //
-// var button_sign_up_closed = document.getElementById("button-sign-up-closed");
+//   btn_sign_up.on('click',function() {
+//     // modal_sign_up.style.display = "block";
+//     openModal(modal_sign_up);
+//   });
 //
-// button_sign_up_closed.onclick = function() {
-//   modal_sign_up.style.display = "none";
+//   // closed button
+//   var btn_closed = $(".button-closed");
+//
+//
+//   function openModal(target) {
+//     target.style.display = 'block';
+//   }
+//
+//   function closeModal() {
+//     this.style.display = 'none';
+//   }
+//
+//
+//   btn_closed.each(function(index, item) {
+//     var $item = $(item);
+//     var $target = $item.parent().parent().parent().parent();
+//     $target.on('click', $.proxy(closeModal, target));
+//     $item.on('click', $.proxy(closeModal, target));
+//   });
+//
+// }(window, window.jQuery));
+
+
+
+// video control
+
+var video_main = document.querySelector(".video-main");
+// console.log('video_main:',video_main);
+
+// video_main.onclick = function() {
+//   play(video_main);
 // }
 
-// closed window
-// var modal_in_bg = document.querySelector(".sign-in-view");
-//
-// modal_in_bg.onclick = function() {
-//   modal_sign_in.style.display = "none";
+  // window.onclick  = video_main.pause;
+  // window.onclick  = video_main.play;
+
+// function video_main() {
+//     video_main.play(onclick);
 // }
 //
-// window.onclick = function(event) {
-//   if (event.target === modal_sign_in) {
-//     modal_sign_in.style.display = "none";
-//   }
+// function video_main() {
+//     video_main.pause(onclick);
 // }
-//
-// window.onclick = function(event) {
-//   if (event.target === modal_sign_up) {
-//     sign_up_modal.style.display = "none";
-//   }
-// }
+
+video_main.addEventListener('click', playControl);
+
+document.addEventListener('keyup', stopVideo);
+
+function stopVideo(e) {
+  if(e.keyCode === 27) {
+    video_main.pause();
+  }
+}
+
+function playControl() {
+  video_main[ video_main.paused ? 'play' : 'pause' ]();
+  // if ( !video_main.paused ) {
+  //     video_main.pause();
+  // } else {
+  //     video_main.play();
+  // }
+}
